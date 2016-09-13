f089ed9d143bd281f4689f91e8a30ecca4365cd1
alanjamesmcdonald <alanjamesmcdonald@gmail.com>
2016-09-13 15:32:47 +0800

    Added index.php

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head profile="http://gmpg.org/xfn/11">
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Damien Flood | Artist/Painter, Ireland</title>
+
+	<meta name="language" content="en-gb" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="description" content="Damien Flood is an Irish artist based in Dublin. His work is grounded in early writings on philosophy, theology, alchemy and the natural sciences." />
+	<meta name="keywords" content="damien flood, art, painting, exhibition, ireland, green on red" />
+	<meta name="classification" content="art, painting, gallery, portfolio" />
+	<meta name="robots" content="index, follow" />
+	<meta name="revisit-after" content="21 days" />
+	<meta name="publisher" content="DAMIEN FLOOD" />
+
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="damienflood.css?ver=2" type="text/css" media="screen" />	
+
+	<script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
+	
+	<script type="text/javascript">
+		// $(document).ready(function() {
+								   
+			// var currentPosition = 0;
+			// var slideWidth = 500;
+			// var slides = $('.slide');
+			// var numberOfSlides = slides.length;
+			// var slideShowInterval;
+			// var fadeInterval;
+			// var speed = 6000;
+			// var currentSlide = 1;
+			
+			// slideShowInterval = setInterval(changePosition, speed);
+			// fadeInterval = setInterval(fadeImages, speed);
+			
+			// slides.wrapAll('<div id="slidesHolder"></div>')
+			
+			// slides.css({ 'float' : 'left' });
+			
+			// $('#slidesHolder').css('width', slideWidth * numberOfSlides);
+			
+			
+			// function changePosition() {
+				// if(currentPosition == numberOfSlides - 1) {
+					// currentPosition = 0;
+				// } else {
+					// currentPosition++;
+				// }
+				// moveSlide();
+			// }
+			
+			
+			// function moveSlide() {
+					// $('#slidesHolder')
+					  // .animate({'marginLeft' : slideWidth*(-currentPosition)});
+			// }
+
+			
+			// function fadeImages() {
+				
+				// fadeout();
+				
+				// if(currentSlide === 5) {
+					// currentSlide = 1;
+				// } else {
+					// currentSlide++;
+				// }					
+				
+				// document.getElementById("image_text").innerHTML = '';
+				
+				// if(currentSlide === 1) {
+					// document.getElementById("image_text").innerHTML = '<p><span style="font-style: italic;">Infinite Plane</span><br/>Grey Noise<br/>Alserkal Avenue<br/>Dubai<br/>25th May - June 31<br/><a href="http://www.greynoise.org" target="_blank">www.greynoise.org</a></p>';
+				// } else if(currentSlide === 2) {
+					// document.getElementById("image_text").innerHTML = '<p><span style="font-style: italic;">The Studio Chronicles</span><br/>RH Contemporary Art<br/>New York<br/>11th June - 5th September<br/><a href="https://www.rhcontemporaryart.com" target="_blank">www.rhcontemporaryart.com</a></p>';
+				// } else if(currentSlide === 3) {
+					// document.getElementById("image_text").innerHTML = '<p><span style="font-style: italic;">Product Recall</span><br/>Galway Arts Centre<br/>Galway<br/>30th May - 4th July<br/><a href=" https://www.galwayartscentre.ie/exhibitions/19-product-recall?lang=en" target="_blank">www.galwayartscentre.ie</a></p>';
+				// } else if(currentSlide === 4) {
+					// document.getElementById("image_text").innerHTML = '<p><span style="font-style: italic;">Inter Nation Art</span><br/>HDLU<br/>Croatia<br/>1st July - 17th July<br/><a href="http://www.hdlu.hr" target="_blank">www.hdlu.hr</a></p>';
+				// } else {
+					// document.getElementById("image_text").innerHTML = '<p><span style="font-style: italic;">RHA Annual Exhibition</span><br/>Royal Hibernian Academy<br/>Dublin<br/>25th May - 8th August<br/><a href="http://www.rhagallery.ie" target="_blank">www.rhagallery.ie</a></p>';
+				// }
+				
+				// fadein();
+			// }
+	
+			// function fadeout(){
+				// $(".slide" + currentSlide).fadeOut(1000);
+			// }
+    
+			// function fadein(){
+				// $(".slide" + currentSlide).fadeIn(1000);
+			// }
+			
+		// });
+	</script>
+</head>
+
+<body>
+	<?php include 'google_analytics.php'; ?>
+	<div id="wrapper">
+		<?php include 'navbar.php'; ?>
+		<!--<div id="sidebar_left">
+		</div>-->
+		<?php include 'sidebar_right.php'; ?>
+		<div id="home_content">
+			<div id="gallery">
+				<img src="images/terra_incognita.jpg" />
+				<p><span style="font-style: italic;">Terra Incognita</span><br/>DLR Lexicon<br/>Marine Road<br/>Dun Laoghaire<br/>8th July - 27th August 2016<br/><a href="http://www.dlrcoco.ie" target="_blank">www.dlrcoco.ie</a></p>
+
+				<!--<div id="image_home">-->
+					<!--<span class="helper"></span><img src="images/globe.jpg" alt="Home page image for Damien Flood, artist/painter, Ireland"  style="padding-right: 90px;" />-->
+					<!--<div id="home_left">
+						<img src="images/Forecast2.jpg" />
+						<p><span style="font-style: italic;">2016: Forecast of the next century</span><br/>The Lewis Glucksman Gallery<br/>University College<br/>Cork<br/>24th March - 3rd July 2016<br/><a href="http://www.glucksman.org" target="_blank">www.glucksman.org</a></p>
+					</div>
+					
+					<div id="home_right">
+						<img src="images/ScribeSOC2.jpg" />
+						<p><span style="font-style: italic;">A Fine Line</span><br/>Green On Red Gallery<br/>Spencer Dock<br/>Park Lane<br/>Dublin 1<br/>Opening 7th April<br/><a href="http://www.greenonredgallery.com" target="_blank">www.greenonredgallery.com</a></p>
+					</div>-->
+
+					
+					<!--<div id="slideshow2">
+						<div id="slideshowWindow2">
+							<div class="slide1">
+								<img src="images/NewWorks.jpg" />
+							</div>
+							
+							<div class="slide1">
+								<img src="images/ScribeSOC.jpg" />
+							</div>
+						</div>
+					</div>-->
+
+					<!--<img src="images/interior_sun_basic_space.jpeg" alt="Interior Sun, C&uacute; Chualainn Comforted, Basic Space" />-->
+					<!--<div id="image_text">
+						<p><span style="font-style: italic;">A Fine Line</span><br/>Green On Red Gallery<br/>Spencer Dock<br/>Park Lane<br/>Dublin 1<br/>Opening 7th April<br/><a href="http://www.greenonredgallery.com" target="_blank">www.greenonredgallery.com</a></p>
+					</div>-->
+					<!--</div>-->
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
